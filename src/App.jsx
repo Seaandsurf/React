d981fc9c8d895002bfce7d5c0728ab07b3245cbc
Seaandsurf <mathias.hardat@gmail.com>
2024-04-03 14:49:29 +0200

    starting rect

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function Square() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </>
+  );
+}
+
+export default Square;
